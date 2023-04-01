93445777176ca53216e3eb6f7bf49754d14bc235
NEIL R. MACABULOS <NEIL2003>
2023-04-01 16:20:58 +0800

    NeilMacabulos2ndSem

---
+++ ECommerce.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿
+using ECommerce.Contracts.Categories;
+using ECommerce.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net.WebSockets;
+
+namespace NeilMacabulos.Prelim2ndSem_.Pages
+{
+    public class Index : PageModel
+    {
+        private readonly ILogger<Index> _logger;
+        private readonly ICategoriesService _categoriesService;
+        public Index(ILogger<Index> logger, ICategoriesService categoriesService)
+        {
+            _logger = logger;
+            _categoriesService = categoriesService;
+        }
+
+        public void OnGet()
+        {
+
+            this.Categories = _categoriesService.GetAll();
+
+        }
+
+        public List<CategoriesDto> Categories { get; set; }
+    }
+}
